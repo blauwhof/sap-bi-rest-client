dd1de7e702959a8e659442bc48619cba5cd4f302
Marco Blauwhof <blauwhof@gmail.com>
2015-10-08 19:52:40 +0200

    added rest client

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Client = require('node-rest-client').Client;
+client = new Client();
 
-var postFunction = function(req, res, next) {
-   console.log('hoi ' + req.body);
+var getFunction = function(req, res, next) {
+   console.log('username ' + req.query.username);
+   console.log('password ' + req.query.password);
+   console.log('username ' + req.query.uri);
+
+      args ={
+        headers:{"Content-Type": "application/xml"},
+        data:"<attrs xmlns='http://www.sap.com/rws/bip'><attr name='userName' type='string'/>" + req.query.username + "</attr><attr name='password' type='string'>" + req.query.password + "</attr><attr name='auth' type='string' possibilities='secEnterprise,secLDAP,secWinAD,secSAPR3'>secEnterprise</attr></attrs>"
+      };
+
+   client.post(req.query.uri + "/logon/long", args, function(data, response){
+               // parsed response body as js object
+               console.log(data);
+               // raw response
+               console.log(response);
+   });
+
 }
 
-router.post('/api', postFunction);
+router.get('/api', getFunction);
 
 module.exports = router;
--- site/js/app.js
+++ site/js/app.js
@@ -3,15 +3,14 @@ $( document ).ready(function() {
     $("#get-data-button").click(function() {
 
         var data = "<iets>hallo</iets>";
-
+        var uri = $("#url").val + '/logon/long'
         $.ajax({
-            type: "POST",
-            url: "http://localhost:3000/api",
-            data: data,
+            type: "GET",
+            url: "http://localhost:3000/api?username=" + $("#username").val() + "&password=" + $("#password").val() + "&uri=" + $("#uri").val(),
+            contentType: "application/json",
             success: function(response) {
                 console.log(response);
-            },
-            dataType: 'xml'
+            }
         });
 
     });
